51072f8e661493d96e8871b4a48f803db21483df
snprajwal <snprajwal@users.noreply.github.com>
2023-12-27 18:21:01 +0000

    deploy: 176d44e4f0661185cfa408d51d095406b9fdd76e

--- bootstrap.js
+++ bootstrap.js
@@ -174,7 +174,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../lost_playground/pkg/lost_playground_bg.wasm":"72d8af9e84d2f4d73d1f"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../lost_playground/pkg/lost_playground_bg.wasm":"433fdde68d6b0fbd987e"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
